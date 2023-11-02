5daeef1a2a0b1d9796b65d8edbb84174dcddabe3
Pietro Albini <pietro@pietroalbini.org>
2023-11-02 12:36:12 +0100

    load files into the linker

---
+++ plink/src/linker/mod.rs
@@ -0,0 +1,93 @@
+use plink_elf::errors::LoadError;
+use plink_elf::ids::serial::SerialIds;
+use plink_elf::{Environment, Object};
+use std::fs::File;
+use std::path::{Path, PathBuf};
+
+pub(crate) struct Linker {
+    ids: SerialIds,
+    first_environment: Option<EnvironmentAndPath>,
+}
+
+impl Linker {
+    pub(crate) fn new() -> Self {
+        Linker {
+            ids: SerialIds::new(),
+            first_environment: None,
+        }
+    }
+
+    pub(crate) fn load_file(&mut self, path: &Path) -> Result<(), LinkerError> {
+        let object = Object::load(
+            &mut File::open(path)
+                .map_err(|e| LinkerError::LoadElfFailed(path.into(), LoadError::IO(e)))?,
+            &mut self.ids,
+        )
+        .map_err(|e| LinkerError::LoadElfFailed(path.into(), e))?;
+
+        self.check_matching_environment(EnvironmentAndPath {
+            env: object.env,
+            path: path.into(),
+        })?;
+
+        Ok(())
+    }
+
+    fn check_matching_environment(
+        &mut self,
+        new_env: EnvironmentAndPath,
+    ) -> Result<(), LinkerError> {
+        match &self.first_environment {
+            Some(first_env) => {
+                if first_env.env != new_env.env {
+                    return Err(LinkerError::MismatchedEnv(first_env.clone(), new_env));
+                }
+            }
+            None => {
+                self.first_environment = Some(new_env);
+            }
+        }
+        Ok(())
+    }
+}
+
+#[derive(Debug, Clone)]
+pub(crate) struct EnvironmentAndPath {
+    env: Environment,
+    path: PathBuf,
+}
+
+#[derive(Debug)]
+pub(crate) enum LinkerError {
+    LoadElfFailed(PathBuf, LoadError),
+    MismatchedEnv(EnvironmentAndPath, EnvironmentAndPath),
+}
+
+impl std::error::Error for LinkerError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            LinkerError::LoadElfFailed(_, err) => Some(err),
+            LinkerError::MismatchedEnv(_, _) => None,
+        }
+    }
+}
+
+impl std::fmt::Display for LinkerError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            LinkerError::LoadElfFailed(path, _) => {
+                write!(f, "failed to load ELF file at {}", path.display())
+            }
+            LinkerError::MismatchedEnv(first, second) => {
+                write!(
+                    f,
+                    "environment of {} is {:?}, while environment of {} is {:?}",
+                    first.path.display(),
+                    first.env,
+                    second.path.display(),
+                    second.env
+                )
+            }
+        }
+    }
+}
--- plink/src/main.rs
+++ plink/src/main.rs
@@ -1,11 +1,17 @@
 use std::error::Error;
 use std::process::ExitCode;
+use crate::linker::Linker;
 
 mod cli;
+mod linker;
 
 fn app() -> Result<(), Box<dyn Error>> {
     let options = cli::parse(std::env::args().skip(1))?;
-    dbg!(options);
+
+    let mut linker = Linker::new();
+    for input in &options.inputs {
+        linker.load_file(input)?;
+    }
 
     Ok(())
 }
@@ -18,7 +24,7 @@ fn main() -> ExitCode {
 
             let mut source = err.source();
             while let Some(s) = source {
-                eprintln!("cause: {s}");
+                eprintln!("caused by: {s}");
                 source = s.source();
             }
 

3d6adeaa76663b98796c26d0992a9b234071b040
Pietro Albini <pietro@pietroalbini.org>
2023-11-12 16:55:53 +0100

    prepare for running 32bit tests

--- plink/tests/linktest/main.rs
+++ plink/tests/linktest/main.rs
@@ -19,69 +19,86 @@ impl Test {
 
         let root = TempDir::new()?;
 
-        for asm in &settings.asm {
-            self.compile_asm(root.path(), asm)?;
+        TestExecution {
+            test: &self,
+            settings: &settings,
+            root: root.path(),
+            suffix: "-64bit",
         }
 
-        match settings.kind {
-            TestKind::LinkFail => self.run_link_fail(settings, root),
-            TestKind::RunFail => self.run_run_fail(settings, root),
-            TestKind::RunPass => self.run_run_pass(settings, root),
+        .run()?;
+
+        Ok(())
+    }
+}
+
+struct TestExecution<'a> {
+    test: &'a Test,
+    settings: &'a TestSettings,
+    root: &'a Path,
+    suffix: &'a str,
+}
+
+impl TestExecution<'_> {
+    fn run(self) -> Result<(), Error> {
+        for asm in &self.settings.asm {
+            self.compile_asm(asm)?;
+        }
+
+        match self.settings.kind {
+            TestKind::LinkFail => self.run_link_fail(),
+            TestKind::RunFail => self.run_run_fail(),
+            TestKind::RunPass => self.run_run_pass(),
         }
     }
 
-    fn run_link_fail(self, settings: TestSettings, root: TempDir) -> Result<(), Error> {
-        if self.link_and_snapshot(&settings, root.path())? {
+    fn run_link_fail(self) -> Result<(), Error> {
+        if self.link_and_snapshot()? {
             bail!("linking was supposed to fail but passed!");
         }
         Ok(())
     }
 
-    fn run_run_fail(self, settings: TestSettings, root: TempDir) -> Result<(), Error> {
-        if self.run_and_snapshot(settings, root.path())? {
+    fn run_run_fail(self) -> Result<(), Error> {
+        if self.run_and_snapshot()? {
             bail!("running was supposed to fail but passed!");
         }
         Ok(())
     }
 
-    fn run_run_pass(self, settings: TestSettings, root: TempDir) -> Result<(), Error> {
-        if !self.run_and_snapshot(settings, root.path())? {
+    fn run_run_pass(self) -> Result<(), Error> {
+        if !self.run_and_snapshot()? {
             bail!("running was supposed to pass but failed!");
         }
         Ok(())
     }
 
-    fn link_and_snapshot(&self, settings: &TestSettings, root: &Path) -> Result<bool, Error> {
-        for debug_print in &settings.debug_print {
-            let outcome = self.link_and_snapshot_inner(settings, root, Some(&debug_print))?;
+    fn link_and_snapshot(&self) -> Result<bool, Error> {
+        for debug_print in &self.settings.debug_print {
+            let outcome = self.link_and_snapshot_inner(Some(&debug_print))?;
             if !outcome {
                 anyhow::bail!("debug printing {debug_print} failed, but should always succeed");
             }
         }
-        self.link_and_snapshot_inner(settings, root, None)
+        self.link_and_snapshot_inner(None)
     }
 
-    fn run_and_snapshot(&self, settings: TestSettings, root: &Path) -> Result<bool, Error> {
-        if !self.link_and_snapshot(&settings, root)? {
+    fn run_and_snapshot(&self) -> Result<bool, Error> {
+        if !self.link_and_snapshot()? {
             bail!("linking was supposed to pass but failed!");
         }
 
-        let mut command = Command::new(root.join("a.out"));
-        command.current_dir(root);
+        let mut command = Command::new(self.root.join("a.out"));
+        command.current_dir(self.root);
 
         self.record_snapshot("run", "running", &mut command, None)
     }
 
-    fn link_and_snapshot_inner(
-        &self,
-        settings: &TestSettings,
-        root: &Path,
-        debug_print: Option<&str>,
-    ) -> Result<bool, Error> {
+    fn link_and_snapshot_inner(&self, debug_print: Option<&str>) -> Result<bool, Error> {
         let mut command = Command::new(env!("CARGO_BIN_EXE_plink"));
         command
-            .current_dir(root)
-            .args(&settings.cmd)
+            .current_dir(self.root)
+            .args(&self.settings.cmd)
             .env("RUST_BACKTRACE", "1");
         if let Some(debug_print) = debug_print {
             command.args(["--debug-print", debug_print]);
@@ -90,8 +107,9 @@ impl Test {
         self.record_snapshot("linker", "linking", &mut command, debug_print)
     }
 
-    fn compile_asm(&self, root: &Path, asm: &AsmFile) -> Result<(), Error> {
+    fn compile_asm(&self, asm: &AsmFile) -> Result<(), Error> {
         let source = self
+            .test
             .files
             .get(&*asm.source)
             .ok_or_else(|| anyhow!("missing {}", asm.source))?;
@@ -107,11 +125,11 @@ impl Test {
             ),
         };
 
-        std::fs::write(root.join(&asm.source), source)?;
+        std::fs::write(self.root.join(&asm.source), source)?;
 
         eprintln!("compiling {} into {dest_name}...", asm.source);
         run(Command::new("nasm")
-            .current_dir(root)
+            .current_dir(self.root)
             .arg("-f")
             .arg(match asm.format {
                 AsmFormat::Elf32 => "elf32",
@@ -131,6 +149,8 @@ impl Test {
         command: &mut Command,
         suffix: Option<&str>,
     ) -> Result<bool, Error> {
+        let snapshot_name = format!("{snapshot_name}{}", self.suffix);
+
         let (output_repr, success) = match command.output() {
             Ok(output) => {
                 let mut output_repr = format!("{action} exited with {}\n", output.status);
@@ -159,7 +179,7 @@ impl Test {
             Path::new(env!("CARGO_MANIFEST_DIR"))
                 .join("tests")
                 .join("linktest")
-                .join(self.name),
+                .join(self.test.name),
         )?);
         if let Some(suffix) = suffix {
             insta_settings.set_snapshot_suffix(suffix);

8acc13c4674e5c52d6451ce75f1779b6d1f5f8cc
Pietro Albini <pietro@pietroalbini.org>
2023-11-19 17:06:01 +0100

    support compiling c programs in linktest

---
+++ plink/linktest/harness/prerequisites/c.rs
@@ -0,0 +1,61 @@
+use crate::utils::run;
+use crate::{TestArch, TestExecution};
+use anyhow::{anyhow, Error};
+use std::path::Path;
+use std::process::Command;
+use tempfile::TempDir;
+
+#[derive(serde::Deserialize)]
+#[serde(deny_unknown_fields)]
+pub(super) struct CFile {
+    source: String,
+    output: Option<String>,
+    libc: Libc,
+}
+
+impl CFile {
+    pub(super) fn build(&self, execution: &TestExecution, dest_dir: &Path) -> Result<(), Error> {
+        let source = execution
+            .test
+            .files
+            .get(&*self.source)
+            .ok_or_else(|| anyhow!("missing {}", self.source))?;
+
+        let dest_name = match &self.output {
+            Some(output) => output.clone(),
+            None => format!(
+                "{}.o",
+                self.source
+                    .rsplit_once('.')
+                    .map(|(name, _ext)| name)
+                    .unwrap_or(&self.source)
+            ),
+        };
+
+        let source_dir = TempDir::new()?;
+        std::fs::write(source_dir.path().join(&self.source), *source)?;
+
+        eprintln!("compiling {} into {dest_name}...", self.source);
+        run(Command::new("cc")
+            .current_dir(source_dir.path())
+            .arg("-c")
+            .arg("-o")
+            .arg(dest_dir.join(dest_name))
+            .arg(match execution.arch {
+                TestArch::X86 => "-m32",
+                TestArch::X86_64 => "-m64",
+            })
+            .args(match self.libc {
+                Libc::Freestanding => &["-nostdlib"],
+            })
+            .arg(&self.source))?;
+
+        Ok(())
+    }
+}
+
+#[derive(serde::Deserialize)]
+#[serde(rename_all = "kebab-case")]
+enum Libc {
+    Freestanding,
+}
--- plink/linktest/harness/prerequisites/mod.rs
+++ plink/linktest/harness/prerequisites/mod.rs
@@ -1,6 +1,8 @@
 mod asm;
+mod c;
 
 use crate::prerequisites::asm::AsmFile;
+use crate::prerequisites::c::CFile;
 use crate::TestExecution;
 use anyhow::Error;
 use std::path::Path;
@@ -10,12 +12,17 @@ use std::path::Path;
 pub(crate) struct Prerequisites {
     #[serde(default)]
     asm: Vec<AsmFile>,
+    #[serde(default)]
+    c: Vec<CFile>,
 }
 
 impl Prerequisites {
     pub(crate) fn build(&self, execution: &TestExecution, dest_dir: &Path) -> Result<(), Error> {
         for asm in &self.asm {
             asm.build(execution, dest_dir)?;
+        }
+        for c in &self.c {
+            c.build(execution, dest_dir)?;
         }
         Ok(())
     }

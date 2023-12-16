6f97b815111131c5bac924dd96f74b7a7c8c10ac
Pietro Albini <pietro@pietroalbini.org>
2023-12-16 12:32:20 +0100

    move debug callbacks impl to a separate module

---
+++ plink/src/debug_print.rs
@@ -0,0 +1,70 @@
+use crate::cli::DebugPrint;
+use crate::linker::{CallbackOutcome, LinkerCallbacks};
+use crate::repr::object::{Object, SectionContent, SectionLayout};
+use plink_elf::ids::serial::SerialIds;
+use plink_elf::ElfObject;
+use std::collections::BTreeMap;
+
+pub(crate) struct DebugCallbacks {
+    pub(crate) print: Option<DebugPrint>,
+}
+
+impl LinkerCallbacks for DebugCallbacks {
+    fn on_inputs_loaded(&self, object: &Object<()>) -> CallbackOutcome {
+        if let Some(DebugPrint::LoadedObject) = self.print {
+            println!("{object:#x?}");
+            CallbackOutcome::Stop
+        } else {
+            CallbackOutcome::Continue
+        }
+    }
+
+    fn on_layout_calculated(&self, object: &Object<SectionLayout>) -> CallbackOutcome {
+        if let Some(DebugPrint::Layout) = self.print {
+            let addresses: BTreeMap<_, _> = object
+                .sections
+                .iter()
+                .map(|(name, section)| {
+                    let addresses: BTreeMap<_, _> = match &section.content {
+                        SectionContent::Data(data) => data
+                            .parts
+                            .iter()
+                            .map(|(id, part)| (id, part.layout.address))
+                            .collect(),
+                        SectionContent::Uninitialized(uninit) => uninit
+                            .iter()
+                            .map(|(id, part)| (id, part.layout.address))
+                            .collect(),
+                    };
+                    (name, addresses)
+                })
+                .collect();
+
+            println!("Section addresses");
+            println!("-----------------");
+            println!("{addresses:#x?}");
+
+            CallbackOutcome::Stop
+        } else {
+            CallbackOutcome::Continue
+        }
+    }
+
+    fn on_relocations_applied(&self, object: &Object<SectionLayout>) -> CallbackOutcome {
+        if let Some(DebugPrint::RelocatedObject) = self.print {
+            println!("{object:#x?}");
+            CallbackOutcome::Stop
+        } else {
+            CallbackOutcome::Continue
+        }
+    }
+
+    fn on_elf_built(&self, elf: &ElfObject<SerialIds>) -> CallbackOutcome {
+        if let Some(DebugPrint::FinalElf) = self.print {
+            println!("{elf:#x?}");
+            CallbackOutcome::Stop
+        } else {
+            CallbackOutcome::Continue
+        }
+    }
+}
--- plink/src/main.rs
+++ plink/src/main.rs
@@ -1,12 +1,10 @@
-use crate::cli::DebugPrint;
-use crate::linker::{link_driver, CallbackOutcome, LinkerCallbacks, LinkerError};
-use crate::repr::object::{Object, SectionContent, SectionLayout};
-use plink_elf::ids::serial::SerialIds;
-use std::collections::BTreeMap;
+use crate::debug_print::DebugCallbacks;
+use crate::linker::{link_driver, LinkerError};
 use std::error::Error;
 use std::process::ExitCode;
 
 mod cli;
+mod debug_print;
 mod interner;
 mod linker;
 mod passes;
@@ -25,70 +23,6 @@ fn app() -> Result<(), Box<dyn Error>> {
     }
 
     Ok(())
-}
-
-struct DebugCallbacks {
-    print: Option<DebugPrint>,
-}
-
-impl LinkerCallbacks for DebugCallbacks {
-    fn on_inputs_loaded(&self, object: &Object<()>) -> CallbackOutcome {
-        if let Some(DebugPrint::LoadedObject) = self.print {
-            println!("{object:#x?}");
-            CallbackOutcome::Stop
-        } else {
-            CallbackOutcome::Continue
-        }
-    }
-
-    fn on_layout_calculated(&self, object: &Object<SectionLayout>) -> CallbackOutcome {
-        if let Some(DebugPrint::Layout) = self.print {
-            let addresses: BTreeMap<_, _> = object
-                .sections
-                .iter()
-                .map(|(name, section)| {
-                    let addresses: BTreeMap<_, _> = match &section.content {
-                        SectionContent::Data(data) => data
-                            .parts
-                            .iter()
-                            .map(|(id, part)| (id, part.layout.address))
-                            .collect(),
-                        SectionContent::Uninitialized(uninit) => uninit
-                            .iter()
-                            .map(|(id, part)| (id, part.layout.address))
-                            .collect(),
-                    };
-                    (name, addresses)
-                })
-                .collect();
-
-            println!("Section addresses");
-            println!("-----------------");
-            println!("{addresses:#x?}");
-
-            CallbackOutcome::Stop
-        } else {
-            CallbackOutcome::Continue
-        }
-    }
-
-    fn on_relocations_applied(&self, object: &Object<SectionLayout>) -> CallbackOutcome {
-        if let Some(DebugPrint::RelocatedObject) = self.print {
-            println!("{object:#x?}");
-            CallbackOutcome::Stop
-        } else {
-            CallbackOutcome::Continue
-        }
-    }
-
-    fn on_elf_built(&self, elf: &plink_elf::ElfObject<SerialIds>) -> CallbackOutcome {
-        if let Some(DebugPrint::FinalElf) = self.print {
-            println!("{elf:#x?}");
-            CallbackOutcome::Stop
-        } else {
-            CallbackOutcome::Continue
-        }
-    }
 }
 
 fn main() -> ExitCode {

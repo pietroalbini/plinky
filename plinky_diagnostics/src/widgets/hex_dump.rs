a0715d6e085fc77fea4341cce41f1edc30bba0a2
Pietro Albini <pietro@pietroalbini.org>
2024-03-18 21:46:31 +0100

    add hex dump widget

---
+++ plinky_diagnostics/src/widgets/hex_dump.rs
@@ -0,0 +1,68 @@
+use crate::widgets::{Table, Widget};
+use crate::WidgetWriter;
+use std::fmt::Write;
+
+const BYTES_PER_LINE: usize = 16;
+
+pub struct HexDump {
+    data: Vec<u8>,
+}
+
+impl HexDump {
+    pub fn new(data: impl Into<Vec<u8>>) -> Self {
+        Self { data: data.into() }
+    }
+}
+
+impl Widget for HexDump {
+    fn render(&self, writer: &mut WidgetWriter<'_>) {
+        let mut hex = String::new();
+        let mut ascii = String::new();
+
+        for chunk in self.data.chunks(BYTES_PER_LINE) {
+            for (i, byte) in chunk.iter().copied().enumerate() {
+                if i > 0 {
+                    hex.push(' ');
+                }
+                hex.write_fmt(format_args!("{byte:0>2x}")).unwrap();
+                if show_as_ascii(byte) {
+                    ascii.push(byte as char);
+                } else {
+                    ascii.push('.');
+                }
+            }
+            hex.push('\n');
+            ascii.push('\n');
+        }
+
+        let mut table = Table::new();
+        table.add_row([hex.trim(), ascii.trim()]);
+        table.render(writer);
+    }
+}
+
+fn show_as_ascii(byte: u8) -> bool {
+    byte.is_ascii_alphanumeric() || byte.is_ascii_punctuation() || byte == b' '
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::configure_insta;
+    use insta::assert_snapshot;
+
+    #[test]
+    fn test_hex_dump_hello() {
+        let _config = configure_insta();
+
+        assert_snapshot!(HexDump::new(b"Hello world").render_to_string());
+    }
+
+    #[test]
+    fn test_hex_dump_256() {
+        let _config = configure_insta();
+
+        let data = (0u8..=255u8).collect::<Vec<_>>();
+        assert_snapshot!(HexDump::new(data).render_to_string());
+    }
+}
--- plinky_diagnostics/src/widgets/mod.rs
+++ plinky_diagnostics/src/widgets/mod.rs
@@ -1,4 +1,5 @@
 mod group;
+mod hex_dump;
 mod quoted_text;
 mod table;
 mod text;
@@ -7,6 +8,7 @@ pub use self::group::WidgetGroup;
 pub use self::quoted_text::QuotedText;
 pub use self::table::Table;
 pub use self::text::Text;
+pub use self::hex_dump::HexDump;
 use crate::WidgetWriter;
 
 pub trait Widget {

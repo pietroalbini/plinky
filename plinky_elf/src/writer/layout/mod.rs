ed364014dc28efe7d94b01ede97593e55f01494b
Pietro Albini <pietro@pietroalbini.org>
2024-08-05 22:28:19 +0200

    use an internal builder struct for the layout

--- plinky_elf/src/writer/layout/mod.rs
+++ plinky_elf/src/writer/layout/mod.rs
@@ -17,150 +17,18 @@ const ALIGN: u64 = 0x1000;
 pub(super) struct WriteLayout<I: ElfIds> {
     parts: Vec<Part<I::SectionId>>,
     metadata: BTreeMap<Part<I::SectionId>, PartMetadata>,
-    current_offset: u64,
     pub(super) header_size: u64,
-    next_padding_id: usize,
 }
 
 impl<I: ElfIds> WriteLayout<I> {
     pub(super) fn new(details: &dyn LayoutDetailsProvider<I>) -> Result<Self, WriteLayoutError> {
-        let mut layout = WriteLayout {
-            parts: Vec::new(),
-            metadata: BTreeMap::new(),
+        let builder = LayoutBuilder {
+            details,
+            layout: WriteLayout { parts: Vec::new(), metadata: BTreeMap::new(), header_size: 0 },
             current_offset: 0,
-            header_size: 0,
             next_padding_id: 0,
         };
-
-        layout.add_part(details, Part::Identification);
-        layout.add_part(details, Part::Header);
-        layout.header_size = layout.current_offset;
-
-        let sections_in_load_segments = details
-            .object()
-            .segments
-            .iter()
-            .enumerate()
-            .filter(|(_idx, s)| matches!(s.type_, ElfSegmentType::Load))
-            .flat_map(|(idx, s)| match &s.content {
-                ElfSegmentContent::Empty => {
-                    Box::new(std::iter::empty()) as Box<dyn Iterator<Item = _>>
-                }
-                ElfSegmentContent::ElfHeader => Box::new(std::iter::empty()),
-                ElfSegmentContent::ProgramHeader => Box::new(std::iter::empty()),
-                ElfSegmentContent::Sections(s) => Box::new(s.iter().map(move |s| (s, idx))),
-                ElfSegmentContent::Unknown(_) => Box::new(std::iter::empty()),
-            })
-            .collect::<BTreeMap<_, _>>();
-
-        // We need to be careful in how sections are written in the resulting ELF file: each LOAD
-        // segment *as a whole* needs to be page aligned, while sections within the segment need to
-        // be contiguous. To address that, we split sections into sections that are not part of any
-        // segment and thus can be written adjacent to each other at the start of the ELF (the
-        // "preamble"), and sections that belong to a segment.
-        //
-        // We then proceed to write all the preamble sections, and after that write all the
-        // segments while being careful of page-aligning each of them.
-        let mut put_in_preamble = Vec::new();
-        let mut put_in_segments = BTreeMap::new();
-        for (id, section) in &details.object().sections {
-            if let Some(segment) = sections_in_load_segments.get(&id) {
-                put_in_segments.entry(*segment).or_insert_with(Vec::new).push((id, section));
-            } else {
-                put_in_preamble.push((id, section));
-            }
-        }
-        for (id, section) in put_in_preamble {
-            layout.add_section(details, id, section)?;
-        }
-        for segment_sections in put_in_segments.values() {
-            layout.align_to_page(details);
-            for (id, section) in segment_sections {
-                layout.add_section(details, id, section)?;
-            }
-        }
-
-        // TODO: waste less space, and try to put the program header next to the elf header.
-        layout.align_to_page(details);
-        layout.add_part(details, Part::ProgramHeaders);
-        layout.add_part(details, Part::SectionHeaders);
-
-        Ok(layout)
-    }
-
-    fn add_section(
-        &mut self,
-        details: &dyn LayoutDetailsProvider<I>,
-        id: &I::SectionId,
-        section: &ElfSection<I>,
-    ) -> Result<(), WriteLayoutError> {
-        match &section.content {
-            ElfSectionContent::Null => {}
-            ElfSectionContent::Program(_) => {
-                self.add_part(details, Part::ProgramSection(id.clone()));
-            }
-            ElfSectionContent::Uninitialized(_) => {
-                // Uninitialized sections are not part of the file layout.
-            }
-            ElfSectionContent::SymbolTable(_) => {
-                self.add_part(details, Part::SymbolTable(id.clone()));
-            }
-            ElfSectionContent::StringTable(_) => {
-                self.add_part(details, Part::StringTable(id.clone()));
-            }
-
-            ElfSectionContent::RelocationsTable(table) => {
-                let mut rela = None;
-                for relocation in &table.relocations {
-                    match rela {
-                        Some(rela) if rela == relocation.addend.is_some() => {}
-                        Some(_) => return Err(WriteLayoutError::MixedRelRela),
-                        None => rela = Some(relocation.addend.is_some()),
-                    }
-                }
-                let rela = rela.unwrap_or(false);
-                if rela {
-                    self.add_part(details, Part::Rela(id.clone()));
-                } else {
-                    self.add_part(details, Part::Rel(id.clone()));
-                }
-            }
-            ElfSectionContent::Group(_) => self.add_part(details, Part::Group(id.clone())),
-            ElfSectionContent::Hash(_) => self.add_part(details, Part::Hash(id.clone())),
-            ElfSectionContent::Dynamic(_) => self.add_part(details, Part::Dynamic(id.clone())),
-
-            ElfSectionContent::Note(_) => {
-                return Err(WriteLayoutError::WritingNotesUnsupported);
-            }
-            ElfSectionContent::Unknown(_) => {
-                return Err(WriteLayoutError::UnknownSection);
-            }
-        }
-        Ok(())
-    }
-
-    fn add_part(&mut self, details: &dyn LayoutDetailsProvider<I>, part: Part<I::SectionId>) {
-        let len = part_len(details, &part) as u64;
-        self.parts.push(part.clone());
-        self.metadata.insert(part, PartMetadata { len, offset: self.current_offset });
-        self.current_offset += len;
-    }
-
-    fn align_to_page(&mut self, details: &dyn LayoutDetailsProvider<I>) {
-        let len = self.len();
-        if len % ALIGN == 0 {
-            return;
-        }
-        let bytes_to_pad = ALIGN - len % ALIGN;
-        self.add_part(
-            details,
-            Part::Padding { id: PaddingId(self.next_padding_id), len: bytes_to_pad as _ },
-        );
-        self.next_padding_id += 1;
-    }
-
-    fn len(&self) -> u64 {
-        self.current_offset
+        builder.build()
     }
 
     pub(super) fn parts(&self) -> &[Part<I::SectionId>] {
@@ -192,6 +60,147 @@ impl<I: ElfIds> WriteLayout<I> {
             .map(|(_, value)| value)
             .next()
             .unwrap()
+    }
+}
+
+struct LayoutBuilder<'a, I: ElfIds> {
+    details: &'a dyn LayoutDetailsProvider<I>,
+    layout: WriteLayout<I>,
+    current_offset: u64,
+    next_padding_id: usize,
+}
+
+impl<I: ElfIds> LayoutBuilder<'_, I> {
+    fn build(mut self) -> Result<WriteLayout<I>, WriteLayoutError> {
+        self.add_part(Part::Identification);
+        self.add_part(Part::Header);
+        self.layout.header_size = self.current_offset;
+
+        let sections_in_load_segments = self
+            .details
+            .object()
+            .segments
+            .iter()
+            .enumerate()
+            .filter(|(_idx, s)| matches!(s.type_, ElfSegmentType::Load))
+            .flat_map(|(idx, s)| match &s.content {
+                ElfSegmentContent::Empty => {
+                    Box::new(std::iter::empty()) as Box<dyn Iterator<Item = _>>
+                }
+                ElfSegmentContent::ElfHeader => Box::new(std::iter::empty()),
+                ElfSegmentContent::ProgramHeader => Box::new(std::iter::empty()),
+                ElfSegmentContent::Sections(s) => Box::new(s.iter().map(move |s| (s, idx))),
+                ElfSegmentContent::Unknown(_) => Box::new(std::iter::empty()),
+            })
+            .collect::<BTreeMap<_, _>>();
+
+        // We need to be careful in how sections are written in the resulting ELF file: each LOAD
+        // segment *as a whole* needs to be page aligned, while sections within the segment need to
+        // be contiguous. To address that, we split sections into sections that are not part of any
+        // segment and thus can be written adjacent to each other at the start of the ELF (the
+        // "preamble"), and sections that belong to a segment.
+        //
+        // We then proceed to write all the preamble sections, and after that write all the
+        // segments while being careful of page-aligning each of them.
+        let mut put_in_preamble = Vec::new();
+        let mut put_in_segments = BTreeMap::new();
+        for (id, section) in &self.details.object().sections {
+            if let Some(segment) = sections_in_load_segments.get(&id) {
+                put_in_segments.entry(*segment).or_insert_with(Vec::new).push((id, section));
+            } else {
+                put_in_preamble.push((id, section));
+            }
+        }
+        for (id, section) in put_in_preamble {
+            self.add_section(id, section)?;
+        }
+        for segment_sections in put_in_segments.values() {
+            self.align_to_page();
+            for (id, section) in segment_sections {
+                self.add_section(id, section)?;
+            }
+        }
+
+        // TODO: waste less space, and try to put the program header next to the elf header.
+        self.align_to_page();
+        self.add_part(Part::ProgramHeaders);
+        self.add_part(Part::SectionHeaders);
+
+        Ok(self.layout)
+    }
+
+    fn add_section(
+        &mut self,
+        id: &I::SectionId,
+        section: &ElfSection<I>,
+    ) -> Result<(), WriteLayoutError> {
+        match &section.content {
+            ElfSectionContent::Null => {}
+            ElfSectionContent::Program(_) => {
+                self.add_part(Part::ProgramSection(id.clone()));
+            }
+            ElfSectionContent::Uninitialized(_) => {
+                // Uninitialized sections are not part of the file layout.
+            }
+            ElfSectionContent::SymbolTable(_) => {
+                self.add_part(Part::SymbolTable(id.clone()));
+            }
+            ElfSectionContent::StringTable(_) => {
+                self.add_part(Part::StringTable(id.clone()));
+            }
+
+            ElfSectionContent::RelocationsTable(table) => {
+                let mut rela = None;
+                for relocation in &table.relocations {
+                    match rela {
+                        Some(rela) if rela == relocation.addend.is_some() => {}
+                        Some(_) => return Err(WriteLayoutError::MixedRelRela),
+                        None => rela = Some(relocation.addend.is_some()),
+                    }
+                }
+                let rela = rela.unwrap_or(false);
+                if rela {
+                    self.add_part(Part::Rela(id.clone()));
+                } else {
+                    self.add_part(Part::Rel(id.clone()));
+                }
+            }
+            ElfSectionContent::Group(_) => self.add_part(Part::Group(id.clone())),
+            ElfSectionContent::Hash(_) => self.add_part(Part::Hash(id.clone())),
+            ElfSectionContent::Dynamic(_) => self.add_part(Part::Dynamic(id.clone())),
+
+            ElfSectionContent::Note(_) => {
+                return Err(WriteLayoutError::WritingNotesUnsupported);
+            }
+            ElfSectionContent::Unknown(_) => {
+                return Err(WriteLayoutError::UnknownSection);
+            }
+        }
+        Ok(())
+    }
+
+    fn add_part(&mut self, part: Part<I::SectionId>) {
+        let len = part_len(self.details, &part) as u64;
+        self.layout.parts.push(part.clone());
+        self.layout.metadata.insert(part, PartMetadata { len, offset: self.current_offset });
+        self.current_offset += len;
+    }
+
+    fn align_to_page(&mut self) {
+        let len = self.len();
+        if len % ALIGN == 0 {
+            return;
+        }
+        let bytes_to_pad = ALIGN - len % ALIGN;
+        self.add_part(Part::Padding {
+            id: PaddingId(self.next_padding_id),
+            len: bytes_to_pad as _,
+        });
+        self.next_padding_id += 1;
+    }
+
+    fn len(&self) -> u64 {
+        self.current_offset
     }
 }
 

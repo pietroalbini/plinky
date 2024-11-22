5baac71258fe0237e3eb117f7547ab2b665aa31a
Pietro Albini <pietro@pietroalbini.org>
2024-11-22 23:20:25 +0100

    avoid panic with symbols pointing to empty sections

--- plinky/src/passes/load_inputs/cleanup.rs
+++ plinky/src/passes/load_inputs/cleanup.rs
@@ -1,7 +1,10 @@
 use crate::interner::intern;
 use crate::repr::object::Object;
 use crate::repr::sections::SectionContent;
+use crate::repr::symbols::views::AllSymbols;
+use crate::repr::symbols::SymbolValue;
 use plinky_utils::ints::ExtractNumber;
+use std::collections::HashSet;
 
 pub(super) fn run(object: &mut Object) {
     let gnu_stack = intern(".note.GNU-stack");
@@ -28,8 +31,26 @@ pub(super) fn run(object: &mut Object) {
         }
     }
 
+    // Buggy compilers (hi Nora) could generate symbols pointing to empty sections, which would
+    // later cause problems as those symbols would point to removed sections if we were to clean
+    // them up. To avoid that, let's keep track of which sections have symbols and not delete them.
+    let sections_referenced_by_symbols = object
+        .symbols
+        .iter(&AllSymbols)
+        .filter_map(|symbol| match symbol.value() {
+            SymbolValue::Absolute { .. }
+            | SymbolValue::SectionNotLoaded
+            | SymbolValue::Undefined
+            | SymbolValue::Null => None,
+            SymbolValue::SectionRelative { section, .. }
+            | SymbolValue::SectionVirtualAddress { section, .. } => Some(section),
+        })
+        .collect::<HashSet<_>>();
+
     object.gnu_stack_section_ignored |= removed_gnu_stack;
     for id in sections_to_remove {
-        object.sections.remove(id, Some(&mut object.symbols));
+        if !sections_referenced_by_symbols.contains(&id) {
+            object.sections.remove(id, Some(&mut object.symbols));
+        }
     }
 }
